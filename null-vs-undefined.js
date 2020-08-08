9d04c3140ed005e4e965f0f5a513f1bd9854f5ee
FAHIM AHMED <fahimfahad35@gmail.com>
2020-08-08 12:22:49 +0600

    null vs undefined clear confusion

---
+++ null-vs-undefined.js
@@ -0,0 +1,27 @@
+let number;
+console.log(number);
+
+
+function add(num1,num2){
+    console.log(num1+num2);
+    return ;
+}
+const result=add(12,23);
+console.log(result);
+
+
+function add(num1,num2){
+    console.log(num1,num2);
+}
+const result=add(12);
+
+
+let premik={fullName:'fahim ahmed',id:1212344,course:'cse'}
+console.log(premik.house);
+
+
+let location=undefined;
+console.log(location);
+
+
+//Null when developer set a value for not existing
