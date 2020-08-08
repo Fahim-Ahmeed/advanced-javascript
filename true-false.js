7fcd060b3b2d941247064061cf9ca2ca581b1b9b
FAHIM AHMED <fahimfahad35@gmail.com>
2020-08-08 11:19:14 +0600

    true false condition

---
+++ true-false.js
@@ -0,0 +1,15 @@
+//falsy
+//0
+//""
+//Null
+//undefined
+//NaN
+//truely
+//'0',' ',{},[]
+const name=[];
+if(name){
+    console.log('condition is true');
+}
+else{
+    console.log('condition is false');
+}
