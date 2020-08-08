f399394a1ec8ad0317a28d19f3820be9229f4cd4
FAHIM AHMED <fahimfahad35@gmail.com>
2020-08-08 12:35:59 +0600

    double vs triple equal confusion clear

---
+++ double-vs-triple.js
@@ -0,0 +1,10 @@
+//double(==)equal means value is equal
+//triple(===)equal means value with data types equal
+let num1=0;
+let num2=false;
+if(num1==num2){
+    console.log('condition is true');
+}
+else{
+    console.log('conditon is false');
+}
